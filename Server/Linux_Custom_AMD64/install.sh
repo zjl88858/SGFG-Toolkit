71b68e526818cc8cfbe980b86b7f9a80896bf841
神龟 <1033008052@qq.com>
2020-10-19 18:40:49 +0800

    Update install.sh

--- Server/Linux_Custom_AMD64/install.sh
+++ Server/Linux_Custom_AMD64/install.sh
@@ -2,3 +2,9 @@
 # Author:Turmi
 # License:WTFPLv2
 # Version:Pre-Release 20201019
+
+echo -e Welcome to "\033[36;1mSGFG Toolkit\033[39;0m"!
+echo Installer will automatic start in 10s.
+echo Press Ctrl+C to abort.
+sleep 10s
+
